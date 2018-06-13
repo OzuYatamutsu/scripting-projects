36ff580d25387bc09eaed49dcc8fdb45e12c45cf
Sean Collins <scollins36@gatech.edu>
2018-06-13 10:28:14 -0400

    Set back to chrome

--- browser_utils.py
+++ browser_utils.py
@@ -1,13 +1,12 @@
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 
 
 class Browser:
     def __init__(self):
-        # self.browser = webdriver.Chrome()
         self.options = Options()
         self.options.set_headless(headless=True)
-        self.browser = webdriver.Firefox(firefox_options=self.options)
+        self.browser = webdriver.Chrome(chrome_options=self.options)
     
     def __enter__(self):
         return self.browser
