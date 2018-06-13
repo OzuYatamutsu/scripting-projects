fef1df3b376ed0c9374269f57289d11701c674c0
Sean Collins <secollins@linkedin.com>
2018-06-13 06:27:37 -0700

    Start hooking up status page

--- scraper_web.py
+++ scraper_web.py
@@ -1,6 +1,13 @@
 from pickle import load
 from time import ctime
 from pprint import pprint
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def render_results():
+    return render_template(hotel_results=load_results())
 
 
 def load_results(file='scraper_results.store') -> dict:
@@ -8,6 +15,7 @@ def load_results(file='scraper_results.store') -> dict:
         return load(f)
 
 if __name__ == '__main__':
-    pprint(f"Last updated: {ctime()}.")
-    print("")
-    pprint(load_results())  # TODO
+    app.run()
+    #pprint(f"Last updated: {ctime()}.")
+    #print("")
+    #pprint(load_results())  # TODO
